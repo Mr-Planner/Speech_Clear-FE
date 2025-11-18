760cf46639b47bd6edb468b597179a28cb3f9949
SangHyun Jung <wjdtkdgus876@naver.com>
2025-11-18 22:49:39 +0900

    feat: path에 따른 SideBar visible 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
@@ -8,6 +8,10 @@ function App() {
 
   const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
+  const location = useLocation(); // 현재 path 경로 정보 가져오기
+  const hideSidebarPaths = ["/login", "/sign-up", "recording"];
+  const showSideBar = !hideSidebarPaths.includes(location.pathname);
+
   const handleToggleSideBar = () => setIsSideBarOpen(prev => !prev);
 
   return (
@@ -15,10 +19,10 @@ function App() {
       <Header/>
 
       <div className="flex flex-1">
-        {isSideBarOpen 
+        {showSideBar&& (isSideBarOpen
           ? <SideBar handleToggleSideBar={handleToggleSideBar} />
           : <HiddenSideBar handleToggleSideBar={handleToggleSideBar} />
-        }
+        )}
 
         <div className="flex-1 overflow-y-auto">
           <Outlet />  
